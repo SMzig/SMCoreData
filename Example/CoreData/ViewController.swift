a6476f1c13397ec3a7f9596211575b15ea8a1991
Sudhir Patel <37684140+SMzig@users.noreply.github.com>
2023-12-05 18:56:26 +0530

    Demo as well

---
+++ Example/CoreData/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by SMzig on 12/05/2023.
+//  Copyright (c) 2023 SMzig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Example/Pods/Target Support Files/CoreData/CoreData-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CoreData : NSObject
+@end
+@implementation PodsDummy_CoreData
+@end
